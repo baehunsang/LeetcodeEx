f1f8f6d7fe8d7661d9656071233df0b58930004a
Bae Hunsang <hoonsang99@korea.ac.kr>
2021-11-21 01:31:03 +0900

    ADD TEMPLIT

---
+++ templet.py
@@ -0,0 +1,21 @@
+import collections
+import functools
+import re
+import sys
+from typing import List
+from typing import Optional, Union
+
+
+class Solution:
+    # Pest your function
+    def fun() -> None:
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    
+    s = Solution()
+    print(s.fun())
