ee628bb747fb253b0f5c9d599fcbaadc8e4b368b
‍배훈상[ 학부휴학 / 컴퓨터학과 ] <hoonsang99@korea.ac.kr>
2021-11-30 12:48:47 +0000

    11/30

---
+++ leetcode/BstToGst.py
@@ -0,0 +1,24 @@
+import collections
+import functools
+import re
+import sys
+from typing import List
+from typing import Optional, Union
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    val: int = 0
+    #setting class variable is more clear.
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        if root:
+            self.bstToGst(root.right)
+            self.val += root.val
+            root.val = self.val
+            self.bstToGst(root.left)
+            return root
+            
+            
